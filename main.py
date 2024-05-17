6fa1e7cad36a4da692c23325ef897034d7901b63
VinkoRobi <Vinkolino178@gmail.com>
2024-05-17 09:49:15 -0500

    Ansley

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import json
 import time 
 from tabulate import tabulate
-const = 'Objetos.json'
+const = 'Infoescuela\Objetos.json'
 def Bienvenida():
     print("Bienvenido a tu inventario. ¿Qué deseas hacer?")
     print("I: Ver inventario")
@@ -20,6 +20,16 @@ def AbrirJson():
         tabla = tabulate(filas, headers=encabezado, tablefmt="grid")
         print(tabla)
 
+def Eliminar():
+    with open (const,'r+') as archivo:
+     data = json.load(archivo)
+     codigo = int(input("Ingresa el código del producto a eliminar: "))
+     for producto in data:
+         if producto['Codigo'] == codigo:
+             data.remove(producto)
+             print("El producto fue elimnado exitosamente")
+    with open(const, "w") as archivo:
+        json.dump(data, archivo, indent=4)
 
 
 
@@ -27,26 +37,36 @@ def VenderProducto():
     with open(const, "r") as archivo:
         data = json.load(archivo)
     codigo = int(input("Ingresa el código del producto a vender: "))
+    codigo2 =  int(input("Ingresa la cantidad que quieres comprar: "))
     for producto in data:
         if producto['Codigo'] == codigo:
             if producto['Stock'] > 0:
-                producto['Stock'] -= 1
+                if(codigo2 >producto['Stock']):
+                    print("Venta no disponible")
+                    break
+                producto['Stock'] -= codigo2
                 Stock = producto["Stock"]
                 print("El precio del producto es:", producto['Precio'])
+                total = producto['Precio']*codigo2
                 print("El Stock es: ", Stock)
+                print("El precio total es ", total)
                 print("Item vendido")
             else:
                 print("No hay stock disponible para este producto.")
             break 
     else:
-        print("Código de producto no válido")
+        print("Codigo de producto no válido")
     with open(const, "w") as archivo:
         json.dump(data, archivo, indent=4)
 
 
 def Quehacer():
     while True:
+        time.sleep(2.5)
         Bienvenida()
+        print("")
+        print("")
+        print("")
         ins = input("¿Qué vas a hacer? ").upper()
         if ins == "I":
             AbrirJson()
@@ -55,6 +75,8 @@ def Quehacer():
             break
         elif ins == "V":
             VenderProducto()
+        elif ins == "E":
+            Eliminar()
         else:
             print("Opción no válida. Por favor, elige una opción válida.")
 
